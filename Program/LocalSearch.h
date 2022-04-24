2dcd92763e0075e88027197be3d8c51cbd94d778
Thibaut Vidal <thibaut.vidal@cirrelt.ca>
2022-04-24 13:43:59 -0400

    Small but useful code optimization in the move evaluations: The code now follows a layered move evaluation strategy, where a large proportion of deteriorating moves are eliminated before even checking the new values of the load and duration constraints. This does not change anything to the behavior of the code and the sequence of solutions produced. Still, with this change, the duration constraints have no impact on the speed of the local search as they are only calculated for a small proportion of moves. We can therefore remove the compilation flag related to them, avoiding any separate compilation process and macro for duration-constrained instances.
    PS: As a side note for teams interested in applying/extending this code to different variants. With this change, it is possible to handle other constraints inside the layered evaluation (as done currently with duration constraints) with very limited impact on LS speed.

--- Program/LocalSearch.h
+++ Program/LocalSearch.h
@@ -141,31 +141,29 @@ private:
 	double loadU, loadX, loadV, loadY;
 	double serviceU, serviceX, serviceV, serviceY;
 	double penaltyCapacityLS, penaltyDurationLS ;
+	bool intraRouteMove ;
 
 	void setLocalVariablesRouteU(); // Initializes some local variables and distances associated to routeU to avoid always querying the same values in the distance matrix
 	void setLocalVariablesRouteV(); // Initializes some local variables and distances associated to routeV to avoid always querying the same values in the distance matrix
 
-	// Functions in charge of excess load and duration penalty calculations
-	 #define penaltyExcessDuration(x) _penaltyExcessDuration(x)
-	// #define penaltyExcessDuration(x) 0. // <--- Use this line instead of the previous one to save some CPU time if your problem does not include duration constraints
-	inline double _penaltyExcessDuration(double myDuration) {return std::max<double>(0., myDuration - params->durationLimit)*penaltyDurationLS;}
+	inline double penaltyExcessDuration(double myDuration) {return std::max<double>(0., myDuration - params->durationLimit)*penaltyDurationLS;}
 	inline double penaltyExcessLoad(double myLoad) {return std::max<double>(0., myLoad - params->vehicleCapacity)*penaltyCapacityLS;}
 
 	/* RELOCATE MOVES */
 	// (Legacy notations: move1...move9 from Prins 2004)
-	bool move1 (); // If U is a client node, remove U and insert it after V
-	bool move2 (); // If U and X are client nodes, remove them and insert (U,X) after V
-	bool move3 (); // If U and X are client nodes, remove them and insert (X,U) after V
+	bool move1(); // If U is a client node, remove U and insert it after V
+	bool move2(); // If U and X are client nodes, remove them and insert (U,X) after V
+	bool move3(); // If U and X are client nodes, remove them and insert (X,U) after V
 
 	/* SWAP MOVES */
-	bool move4 (); // If U and V are client nodes, swap U and V
-	bool move5 (); // If U, X and V are client nodes, swap (U,X) and V
-	bool move6 (); // If (U,X) and (V,Y) are client nodes, swap (U,X) and (V,Y) 
+	bool move4(); // If U and V are client nodes, swap U and V
+	bool move5(); // If U, X and V are client nodes, swap (U,X) and V
+	bool move6(); // If (U,X) and (V,Y) are client nodes, swap (U,X) and (V,Y) 
 	 
 	/* 2-OPT and 2-OPT* MOVES */
-	bool move7 (); // If route(U) == route(V), replace (U,X) and (V,Y) by (U,V) and (X,Y)
-	bool move8 (); // If route(U) != route(V), replace (U,X) and (V,Y) by (U,V) and (X,Y)
-	bool move9 (); // If route(U) != route(V), replace (U,X) and (V,Y) by (U,Y) and (V,X)
+	bool move7(); // If route(U) == route(V), replace (U,X) and (V,Y) by (U,V) and (X,Y)
+	bool move8(); // If route(U) != route(V), replace (U,X) and (V,Y) by (U,V) and (X,Y)
+	bool move9(); // If route(U) != route(V), replace (U,X) and (V,Y) by (U,Y) and (V,X)
 
 	/* SUB-ROUTINES FOR EFFICIENT SWAP* EVALUATIONS */
 	bool swapStar(); // Calculates all SWAP* between routeU and routeV and apply the best improving move
